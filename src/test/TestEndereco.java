9c0cff723ae3455f6a3fcb7ea87b130dfbd3d9f8
Marcella Costa <48464412+MarceCM@users.noreply.github.com>
2023-06-04 20:39:03 -0300

    Create TestEndereco.java

---
+++ src/test/TestEndereco.java
@@ -0,0 +1,19 @@
+package test.java.org.fpij.jitakyoei.model.beans;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.fpij.jitakyoei.model.beans.Endereco;
+
+public class EnderecoTest {
+    @Test
+	public void cepComLetraTest() {
+		// initial setup
+		Endereco endereco = new Endereco();
+		String expected = endereco.getCep();
+		endereco.setCep("123456mn");
+		
+		String curr_value = endereco.getCep();
+		assertEquals(expected, curr_value);
+	}
+}
